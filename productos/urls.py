82d3b06de9c5b2191f801fa78de1e07bb44369ad
Trejo Duque Alex Fernando <lanchado10@gmail.com>
2024-06-07 02:01:07 -0500

    Laboratorio Jinja 2 con Django v 1.5.0

--- productos/urls.py
+++ productos/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('crear/', views.crear_producto, name='crear_producto'),
     path('<int:pk>/editar', views.editar_producto, name='editar_producto'),
     path('<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
+    path('exportar/csv/', views.exportar_productos_csv, name='exportar_productos_csv'),
+    path('importar/csv/', views.importar_productos_csv, name='importar_productos_csv'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,10 @@
+import csv
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from .models import Producto
 from .forms import ProductoForm
+from django.db.models import Q
+
 
 productos = []
 
@@ -47,3 +51,55 @@ def eliminar_producto(request, pk):
         elif 'cancelar' in request.POST:
             return redirect('listar_productos')
     return render(request, 'eliminar.html', {'producto': producto})
+
+
+def exportar_productos_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="productos.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Nombre', 'Precio', 'Cantidad'])
+    productos = Producto.objects.all()
+    for producto in productos:
+        writer.writerow([producto.id, producto.nombre, producto.precio, producto.cantidad])
+    return response
+
+def importar_productos_csv(request):
+    if request.method == 'POST' and request.FILES['csv_file']:
+        csv_file = request.FILES['csv_file']
+        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        
+        # Verificar que hay al menos dos filas en el archivo CSV
+        if len(decoded_file) < 2:
+            return HttpResponse("El archivo CSV no contiene suficientes filas para importar.")
+        
+        reader = csv.reader(decoded_file)
+        next(reader)  # Saltar la fila de encabezados
+        
+        # Lista para almacenar nombres de productos del CSV
+        productos_csv = set()
+        
+        for row in reader:
+            if len(row) >= 4:  # Verificar si hay suficientes elementos en la fila
+                nombre_producto = row[1]
+                productos_csv.add(nombre_producto)
+                
+                # Buscar el producto por su nombre
+                producto, _ = Producto.objects.get_or_create(
+                    nombre=nombre_producto,
+                    defaults={
+                        'precio': row[2],
+                        'cantidad': row[3]
+                    }
+                )
+                # Si el producto ya existe, actualizar sus detalles
+                if not _:
+                    producto.precio = row[2]
+                    producto.cantidad = row[3]
+                    producto.save()
+        
+        # Eliminar los productos que no están en el archivo CSV
+        productos_a_eliminar = Producto.objects.filter(~Q(nombre__in=productos_csv))
+        productos_a_eliminar.delete()
+                    
+        return redirect('listar_productos')
+    return render(request, 'importar_productos.html')
